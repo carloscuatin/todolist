26d15aeaceb6ef44531595cfe0a0e844bc371c86
Carlos Cuatin <cecg1996@gmail.com>
2015-10-11 17:33:45 -0500

    add linters and config

---
+++ src/app/client/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by carlos on 27/09/15.
+ */
+import Client from 'react-engine/lib/client';
+import routes from '../shared/routes.js';
+
+// -- Boot Options
+const options = { routes };
+
+document.addEventListener('DOMContentLoaded', function onLoad() {
+  Client.boot(options);
+});
---
+++ src/app/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by carlos on 27/09/15.
+ */
+const config = {
+  app: {
+    name: 'Carlos Cuatin, Front End Developer',
+    port: proccess.env.PORT || 3000
+  },
+
+  api: 'http://localhost:3001'
+}
+
+export default config;
